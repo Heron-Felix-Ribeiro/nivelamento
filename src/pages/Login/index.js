89e3fbe88b0fafad36b0045901713fa9c170e0eb
Heron Felix Ribeiro <heronfelixribeiro@gmail.com>
2025-03-12 22:15:46 -0300

    Aula 12/3

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,41 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { UsuarioContext, useUsuarioContext } from "../../contexts/Usuario";
 
 
 export default function Login() {
 
+    const [usuarioInformado, setUsuarios] = useState("");
+    const [senha, setSenha] = useState("");
+    const {login} = useUsuarioContext(UsuarioContext);
+    const navigate = useNavigate(); 
+
+    function loginSubmit (e) {
+        e.preventDefault();
+
+        debugger;
+        if(usuarioInformado === "Heron" && senha === "1"){
+            login({nome: usuarioInformado, usuarioInformado, logado: true });
+            navigate("/")
+        }
+        else{
+            alert("Ta errado")
+        }    
+    
+
+    }
+
     return (
-        <div className="container md-6 bg-dark text-light">
+        <form onSubmit={loginSubmit}>
             <h1 className="text-center fw-bold mt-2">Login</h1>
             <div className="container mt-5 bg-dark pb-5">
 
                 <label className="text-light">Usuario: </label>
-                <input className="form-control"></input>
+                <input type="text" value={usuarioInformado} onChange={(e) => setUsuarios(e.target.value)} className="form-control"></input>
                 <label className="text-light">Senha: </label>
-                <input className="form-control"></input>
-                <button className="btn btn-primary mt-2 col-md-2 text-center">Logar</button>
+                <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} className="form-control"></input>
+                <button type="submit" className="btn btn-primary mt-2 col-md-2 text-center w-100">Entrar</button>
             </div>
-        </div>
+        </form>
     )
 }
--- src/route.js
+++ src/route.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Menu from "./pages/Menu";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -9,30 +9,59 @@ import Despesas from "./pages/Despesas";
 import Login from "./pages/Login";
 import CriarTransacao from "./pages/CriarTransacao";
 import CriarDespesa from "./pages/CriarDespesa";
+import UsuarioLogadoProvider, { UsuarioContext } from "./contexts/Usuario"
+import { useContext } from "react";
+
+function PrivateRoute({ children }) {
+    const usuario = useContext(UsuarioContext)
+
+    if (!usuario["usuario"]?.logado) {
+
+        return (
+            <Navigate to="/login" replace />
+        )
+
+
+    }
+
+    return children;
+}
 
 export default function AppRoute() {
 
     return (
         <BrowserRouter>
-            <Header />
-            <div className="d-flex">
-                <Sidebar />
+            <UsuarioLogadoProvider>
 
-                <div className="flex-grow-1 p-4">
+                <Routes>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/*"
+                        element={
+                            <PrivateRoute>
+                                <Routes>
+                                    <Route path="/" element={<Menu />}></Route>
+                                    <Route path="/cadastro" element={<Cadastro />}></Route>
+                                    <Route path="/transacoes" element={<Transacoes />}></Route>
+                                    <Route path="/despesas" element={<Despesas />}></Route>
+                                    <Route path="/criar_transacao" element={<CriarTransacao />}></Route>
+                                    <Route path="/criar_tipo_despesa" element={<CriarDespesa />}></Route>
+                                </Routes>
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
 
-                    <Routes>
-                        <Route path="/" element={<Menu />}></Route>
-                        <Route path="/cadastro" element={<Cadastro />}></Route>
-                        <Route path="/transacoes" element={<Transacoes />}></Route>
-                        <Route path="/despesas" element={<Despesas />}></Route>
-                        <Route path="/login" element={<Login />}></Route>
-                        <Route path="/criar_transacao" element={<CriarTransacao />}></Route>
-                        <Route path="/criar_tipo_despesa" element={<CriarDespesa />}></Route>
-                    </Routes>
-
-                </div>
-            </div>
-            <Footer />
+            </UsuarioLogadoProvider>
         </BrowserRouter>
     )
 }
+
+//<Header />
+//                <div className="d-flex">
+//                   <Sidebar />
+//
+//                   <div className="flex-grow-1 p-4"></div>
+
+//</div>
+//</div>
+//<Footer />
