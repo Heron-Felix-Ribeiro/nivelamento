b777d6d8cfe177f59e78e51d3458cb34f3d09e01
Heron Felix Ribeiro <heronfelixribeiro@gmail.com>
2025-04-25 22:01:11 -0300

    25/04 /2

--- front/src/route.js
+++ front/src/route.js
@@ -15,7 +15,7 @@ import { Provider, useSelector } from "react-redux";
 import store from './redux/store'
 
 function PrivateRoute({ children }) {
-    const token = useSelector(state=> state.auth.token)
+    const token = useSelector(state => state.auth.token)
 
         return token? children : <Navigate to="/login" replace />
 
--- front/src/service/api.js
+++ front/src/service/api.js
@@ -1,8 +1,19 @@
 import axios from "axios";
+import store from "../redux/store";
 
 const api = axios.create({
     baseURL:'http://localhost:8080/'
 });
 
+api.interceptors.request.use ((config) => {
+    
+    const token = store.getState().auth.token; 
+
+    if(token && !config.url.includes('/login')){
+        config.headers.Authorization = 'Bearer ${token}';
+    }
+
+    return config; 
+})
 
 export default api; 
